21a65e39e57632a7a2df8e2eccc95d04c7e1b554
surendarsurendar <35688306+surendarsurendar@users.noreply.github.com>
2018-02-02 13:57:05 +0530

    surendar2.c

---
+++ surensdar2.c
@@ -0,0 +1,21 @@
+#include<stdio.>
+void main()
+{
+int a,b,c;
+printf("enter three numbers:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b && a>c)
+{
+    printf("%d",a);
+}
+else if(b>a && b>c)
+{
+    printf("%d",b);
+}
+else if(c>a && c>b)
+{
+    printf("%d",c);
+}
+getch();
+return 0;
+}
